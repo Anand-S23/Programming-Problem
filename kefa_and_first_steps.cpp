7286860425d538668e5905de2180f4cf499dac50
ASingh23 <anandsroopsingh23@gmail.com>
2020-10-17 17:15:01 -0400

    Fix bug in kefa_and_first_steps

--- kefa_and_first_steps.cpp
+++ kefa_and_first_steps.cpp
@@ -7,33 +7,35 @@ int main()
     int n; 
     int current_count = 0; 
     int max = 0;
+    int a, prev; 
 
-    cin << n;
+    cin >> n;
 
     for (int i = 0; i < n; ++i)
     {
-        int a, prev; 
-        cin << a; 
+        cin >> a; 
 
         if (i != 0)
         {
             if (a < prev)
             {
-                if (current_count > max)
-                {
-                    max = current_count; 
-                }
-
-                current_cout = 0; 
+                current_count = 0; 
             }
             else
             {
                 current_count++;
             }
+
+            if (current_count > max)
+            {
+                max = current_count; 
+            }
         }
 
         prev = a;
     }
+
+    cout << ++max;
 
     return 0;
 }
